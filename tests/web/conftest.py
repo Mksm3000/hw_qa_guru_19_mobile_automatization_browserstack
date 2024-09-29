15975948b87aceb2e14a0c5f8faffdb49e41386f
Maksim Zosimov <mizo-y@yandex.ru>
2024-09-29 11:36:40 +0200

    add selenoid

--- tests/web/conftest.py
+++ tests/web/conftest.py
@@ -1,25 +1,46 @@
 import pytest
 from selene import browser
 import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from utils.attach import png_attachment, log_attachment, html_attachment, web_video_attachment
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     browser.config.base_url = os.getenv('base_url', 'https://www.wikipedia.org')
-    browser.config.driver_name = os.getenv('driver_name', 'chrome')
 
-    # from selenium import webdriver
-    # chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument('--headless')
-    # browser.config.driver_options = chrome_options
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    browser.config.timeout = 30
 
-    browser.config.hold_driver_at_exit = (
-        os.getenv('hold_driver_at_exit', 'false').lower() == 'true'
+    selenoid_login = os.getenv('SELENOID_LOGIN')
+    selenoid_pass = os.getenv('SELENOID_PASS')
+    selenoid_url = os.getenv('SELENOID_URL')
+
+    options = Options()
+    selenoid_capabilities = {
+        'browserName': 'chrome',
+        'browserVersion': '126.0',
+        'selenoid:options': {
+            'enableVNC': True,
+            'enableVideo': True,
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    driver = webdriver.Remote(
+        command_executor=f'https://{selenoid_login}:{selenoid_pass}@{selenoid_url}',
+        options=options
     )
-    browser.config.window_width = os.getenv('window_width', '1920')
-    browser.config.window_height = os.getenv('window_height', '1080')
-    browser.config.timeout = float(os.getenv('timeout', '4.0'))
 
-    yield
+    browser.config.driver = driver
+
+    yield browser
+
+    png_attachment(browser)
+    log_attachment(browser)
+    html_attachment(browser)
+    web_video_attachment(browser)
 
     browser.quit()
--- utils/attach.py
+++ utils/attach.py
@@ -11,9 +11,37 @@ def png_attachment(browser):
 
 
 def xml_attachment(browser):
-    allure.attach(browser.driver.page_source,
+    allure.attach(source=browser.driver.page_source,
                   name='screen xml dump',
-                  attachment_type=allure.attachment_type.XML)
+                  attachment_type=allure.attachment_type.XML,
+                  extension='.xml')
+
+
+def log_attachment(browser):
+    log = "".join(f'{text}\n' for text in browser.driver.get_log(log_type='browser'))
+    allure.attach(source=log,
+                  name='browser_logs',
+                  attachment_type=allure.attachment_type.TEXT,
+                  extension='.log')
+
+
+def html_attachment(browser):
+    html = browser.driver.page_source
+    allure.attach(source=html,
+                  name='page_source',
+                  attachment_type=allure.attachment_type.HTML,
+                  extension='.html')
+
+
+def web_video_attachment(browser):
+    video_url = "https://selenoid.autotests.cloud/video/" + browser.driver.session_id + ".mp4"
+    html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
+           + video_url \
+           + "' type='video/mp4'></video></body></html>"
+    allure.attach(source=html,
+                  name='video_' + browser.driver.session_id,
+                  attachment_type=allure.attachment_type.HTML,
+                  extension='.html')
 
 
 def video_attachment(session_id):
